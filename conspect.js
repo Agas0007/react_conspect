c8df313bfe42abaae1a14dea8749e9a6830b64db
Andrey Babaryka <BabarykaSES@yandex.ru>
2024-07-01 18:03:34 +0300

    Пополнение

--- conspect.js
+++ conspect.js
@@ -16,7 +16,34 @@ useEffect(() => {
 }
 
 
+/* *** или добавить класс hidden body  **** */
+
+ useEffect(() => {
+    document.body.classList.toggle("body_hidden", stateCatalog);
+  }, [stateCatalog]);
+
+
 
 /****
 ** 
 ****************************/
+
+
+  /*-закрыть меню при клике вне меню-*/
+
+  const removeClassMenu = (e) => {
+    if (!e.target.closest(".header_catalog_btn, .header_catalog, .header_user_login, .login_box, .humb, .become_seller")) {
+      document.querySelector(".header_catalog").classList.remove("header_catalog_visible");
+      document.querySelector(".header_catalog_btn").classList.remove("catalog_btn_active");
+      document.querySelector(".login_box").classList.remove("login_box_active");
+      document.querySelector(".login_box_salesman").classList.remove("login_box_active");
+      document.querySelector("body").classList.remove("body_hidden");
+    }
+  };
+
+  useEffect(() => {
+    document.body.addEventListener("click", removeClassMenu);
+    return () => document.body.removeEventListener("click", removeClassMenu);
+  });
+
+  /* END закрыть меню при клике вне меню-*/
